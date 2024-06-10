e565aae8654c38d927ce473cd2fab4772a8d88e4
ITdevkc <132387209+ITdevkc@users.noreply.github.com>
2024-06-10 19:03:52 -0400

    Use PRVM_ED_ParseEdict_OnLoad for loading saves

--- sv_save.c
+++ sv_save.c
@@ -434,7 +434,7 @@ void SV_Loadgame_f(cmd_state_t *cmd)
 			if(developer_entityparsing.integer)
 				Con_Printf("SV_Loadgame_f: loading edict %d\n", entnum);
 
-			PRVM_ED_ParseEdict (prog, start, ent);
+			PRVM_ED_ParseEdict_OnLoad (prog, start, ent);
 
 			// link it into the bsp tree
 			if (!ent->free)
